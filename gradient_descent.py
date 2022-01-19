c6cb9c66fa900d34df1bcfb26339d2272ec885cb
RoyGeorge29 <roygeorgekala@gmail.com>
2022-01-19 12:41:31 +0530

    Basic Algorithm

--- gradient_descent.py
+++ gradient_descent.py
@@ -2,6 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
+
+def gradient_descent(gradient, start, learning_rate, iteration_count):
+
+    vector = start
+    while iteration_count > 0:
+        iteration_count -= 1
+        update_value = learning_rate * (-1*gradient(vector))
+        vector += update_value
+    return vector
+
+
 x = np.array([x for x in range(50)])
 y = np.array([x - random.randint(0, 2) for x in range(50)])
 
