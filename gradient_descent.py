0004124113ab081a7c0ea2d9b02fb98328e63b33
RoyGeorge29 <roygeorgekala@gmail.com>
2022-01-19 15:31:07 +0530

    Finished Gradient Descent

--- gradient_descent.py
+++ gradient_descent.py
@@ -3,18 +3,16 @@ import numpy as np
 import random
 
 
-def gradient_descent(gradient, start, learning_rate, iteration_count):
+def gradient_descent(gradient, start, learning_rate, iteration_count=50, tolerance=1e-06):
 
     vector = start
     while iteration_count > 0:
         iteration_count -= 1
         update_value = learning_rate * (-1*gradient(vector))
+        if np.all(np.abs(update_value) <= tolerance):
+            break
         vector += update_value
     return vector
 
 
-x = np.array([x for x in range(50)])
-y = np.array([x - random.randint(0, 2) for x in range(50)])
-
-plt.plot(x, y, 'ro')
-plt.show()
+print(gradient_descent(lambda x: 2*x, start=10, learning_rate=0.1))
